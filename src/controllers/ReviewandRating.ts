e30cc338db09120af6d7744292b73fe368490099
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-09-08 01:54:40 +0530

    wip

--- src/controllers/ReviewandRating.ts
+++ src/controllers/ReviewandRating.ts
@@ -4,22 +4,22 @@ import RatinAndReview from "../models/RatinAndReview";
 import User from "../models/User";
 
 
-export const addReviewAndRating = async (req : Request , res : Response) => {
-    const { rating , review, productId , userId } = req.body;
+export const addReviewAndRating = async (req: Request, res: Response) => {
+    const { rating, review, productId, userId } = req.body;
     try {
 
         const user = await User.findById(userId);
 
         const addRating = await RatinAndReview.create({
-            user : user?._id,
+            user: user?._id,
             rating,
             review
         });
 
-        await Product.findByIdAndUpdate(productId , {review : addRating._id }, {new : true});
+        await Product.findByIdAndUpdate(productId, { review: addRating._id }, { new: true });
         res.status(200).json({
-            message : "Ratings added Successfully !",
-            rating : addRating
+            message: "Ratings added Successfully !",
+            rating: addRating
         })
     } catch (e) {
         console.log(e);
